a662bd31b6e90f9a3d6c4f27c23d86b4838e6472
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-15 09:41:33 -0400

    now to test if this works from the console

--- json_helper.py
+++ json_helper.py
@@ -1,10 +1,27 @@
-import json, os, pickle
+import json, os, pickle, sys
 
 
 def read_json(file_path):
     file = open(file_path)
     data = json.loads(file.read())
     return data
+
+def is_json(file_name):
+    return '.json' in file_name
+
+def is_pickle(file_name):
+    return '.pickle' in file_name
+
+def is_directory(file_path):
+    return os.path.isdir(file_path)
+
+def get_pickle_title(file_path):
+    dir_list = file_path.split("/")
+    if file_path[-1] == "/":
+        return dir_list[-2]
+    else:
+        return dir_list[-1]
+
 
 
 #return a list w/ json objects
@@ -36,8 +53,8 @@ def read_all_json(dirName):
     return json_files
 
 
-def write_pickle(file_path, data_to_pickle):
-    with open(file_path + '/super_smash_characters.pickle', 'wb') as f:
+def write_pickle(file_path, file_name, data_to_pickle):
+    with open(file_path + str(file_name), 'wb') as f:
         pickle.dump(data_to_pickle, f)
 
 
@@ -55,6 +72,9 @@ def load_pickle(file_path):
 data_to_write = {"name:": "Goku", "race": "Saiyan", "Family": "ChiChi and Gohan", "hype move": "spirit bomb"}
 path_to_write = '/Users/nick/Python/Labs/PythonFundamentals.Exercises.Part9/data/dragon_ball_z/'
 
+print(get_pickle_title(path_to_write))
+
+
 with open(path_to_write + 'Goku.json', 'w') as file_to_write:
     json.dump(data_to_write, file_to_write)
 
@@ -65,3 +85,20 @@ pickle_path = "/Users/nick/Python/Labs/PythonFundamentals.Exercises.Part9/data/s
 write_pickle(pickle_path, data)
 pickle_name = "/Users/nick/Python/Labs/PythonFundamentals.Exercises.Part9/data/super_smash_bros/super_smash_characters.pickle"
 print(load_pickle(pickle_name))
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("please include path/filename or path")
+        exit()
+    if len(sys.argv) == 2:
+        if is_json(sys.argv[1]):
+            print(read_json(sys.argv[1]))
+        elif is_pickle(sys.argv[1]):
+            print(load_pickle(sys.argv[1]))
+        elif is_directory(sys.argv[1]):
+            data = read_all_json(sys.argv[1])
+            file_name = get_pickle_title(sys.argv[1])
+            write_pickle(sys.argv[1], file_name, data)
+            print("pickling complete!")
+        else:
+            print("please select file with valid extension")
